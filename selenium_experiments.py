1f5d8f4953dbf7ab27e82f53fb253a465537e3fe
Fehniix <29045904+Fehniix@users.noreply.github.com>
2020-12-22 23:29:44 +0100

    The script is now able to close Adblock, disable tutorial and start the game.

--- selenium_experiments.py
+++ selenium_experiments.py
@@ -4,8 +4,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-
 import time
+import logging as logger
+
+# Default logger level (root) is set to WARNING. `logging` ignores any level below it.
+logger.root.setLevel(level=logger.NOTSET)
+
+# POST over a JavaScript script to the current tab and return the readyState
+def getPageState(driver):        
+    return driver.execute_script('return document.readyState')
+
+def getAvailableShapes(driver):
+	return driver.execute_script('return PlayState.shapes;')
 
 # goog:loggingPrefs allows for access to console logs.
 desiredCapabilities = DesiredCapabilities.CHROME
@@ -23,15 +33,30 @@ driver = webdriver.Chrome(desired_capabilities=desiredCapabilities, options=chro
 WebDriverWait(driver, 2)
 
 # switch to it, close it and switch target window back to first.
-driver.switch_to_window(driver.window_handles[1])
+driver.switch_to.window(driver.window_handles[1])
 driver.close()
-driver.switch_to_window(driver.window_handles[0])
+driver.switch_to.window(driver.window_handles[0])
 
 # Load the game page.
 driver.get("https://games.gamesplaza.com/ext/distribution/wood_blocks/index.html?dist.version=2&v1")
 
+# Sleep until the document is ready.
+while(getPageState(driver) != 'complete'):
+	time.sleep(0.050)
+
+# Let's not go through the tutorial each single time, ay?
+# No, seriously, set the tutorialCompleted property to true so that the tutorial doesn't fire.
+driver.execute_script('R.playerData.tutorialCompleted = true;')
+
+# Wait for game to load
+time.sleep(7)
+
+# Start the game!
+driver.execute_script('game.state.start(\'play\')')
+
 # Dumb logging. Needs to ideally be a thread continuously polling for logs.
-while(True):
+debug = False
+while(debug):
 	for entry in driver.get_log('browser'):
 		print(entry)
 	time.sleep(100)
