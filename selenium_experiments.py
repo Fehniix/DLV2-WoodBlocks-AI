da1b8f57babb522d0f7c3ef2ae3a0c7def355537
MikeCoder96 <bncmhl96r23a773d@studenti.unical.it>
2020-12-23 12:33:49 +0100

    get shapes that we need to put in the matrix

--- selenium_experiments.py
+++ selenium_experiments.py
@@ -1,9 +1,23 @@
+import time
 import chrome_driver
 import msedge_driver
-import time
 
 ENABLE_ADBLOCK = False
 BROWSER_TO_USE = 1
+MATRIX_LENGTH = 18
+INITIAL_MATRIX = [[0 for x in range(MATRIX_LENGTH)] for y in range(MATRIX_LENGTH)] 
+SHAPE_0 = None
+SHAPE_1 = None
+SHAPE_2 = None
+
+def getShapes():
+	global SHAPE_0
+	global SHAPE_1
+	global SHAPE_2
+	
+	SHAPE_0 = driver.execute_script("return PlayState.shapes[0].state")
+	SHAPE_1 = driver.execute_script("return PlayState.shapes[1].state")
+	SHAPE_2 = driver.execute_script("return PlayState.shapes[2].state")
 
 if BROWSER_TO_USE == 0:
 	driver = chrome_driver.createChromeDriver(ENABLE_ADBLOCK)
@@ -20,9 +34,14 @@ if ENABLE_ADBLOCK:
 
 # Load the game page.
 driver.get("https://games.gamesplaza.com/ext/distribution/wood_blocks/index.html?dist.version=2&v1")
+driver.execute_script('R.playerData.tutorialCompleted = true;') #Just skip this f*****g tutorial
+input("Press any key to start the game....")
+
+getShapes()
 
 # Dumb logging. Needs to ideally be a thread continuously polling for logs.
-while(True):
-	for entry in driver.get_log('browser'):
-		print(entry)
-	time.sleep(100)
+#while(True):
+#	for entry in driver.get_log('browser'):
+#		print(entry)
+#	time.sleep(100)
+
